f7a8769b292166ff80fe10437ae2f25ddd5232f6
Kafeel <101697894+Sharky1507@users.noreply.github.com>
2024-06-02 16:00:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import streamlit as st
-from pytube.__main__ import YouTube
-import subprocess
-st.title("Youtube Video Downloader")
+from pytube import YouTube
+import os
+
+st.title("YouTube Video Downloader")
 st.subheader("Enter the URL:")
 url = st.text_input(label='URL')
-if url != '':
+
+if url:
     yt = YouTube(url)
     st.image(yt.thumbnail_url, width=300)
     st.subheader('''
@@ -12,22 +14,34 @@ if url != '':
     ## Length: {} seconds
     ## Rating: {} 
     '''.format(yt.title , yt.length , yt.rating))
-    video = yt.streams
-    if len(video) > 0:
-        downloaded , download_audio = False , False
-        download_video = st.button("Download Video")
-        if yt.streams.filter(only_audio=True):
-            download_audio = st.button("Download Audio Only")
-        if download_video:
-            video.get_lowest_resolution().download()
-            downloaded = True
-        if download_audio:
-            video.filter(only_audio=True).first().download()
-            downloaded = True
-        if downloaded:
-            st.subheader("Download Complete")
-            subprocess.run(["dir"],shell  =True)
-    else:
-        st.subheader("Sorry, this video can not be downloaded")
 
-        
+    video_streams = yt.streams
+    if video_streams:
+        download_choice = st.radio(
+            "Choose format to download",
+            ('Video', 'Audio')
+        )
+
+        #download_button = st.button("Download")
+
+        #if download_button:
+        if download_choice == 'Video':
+                video_file = video_streams.get_lowest_resolution().download()
+                with open(video_file, 'rb') as file:
+                    st.download_button(
+                        label="File is ready! Click here to download the video",
+                        data=file,
+                        file_name=os.path.basename(video_file),
+                        mime='video/mp4'
+                    )
+        elif download_choice == 'Audio':
+                audio_file = video_streams.filter(only_audio=True).first().download()
+                with open(audio_file, 'rb') as file:
+                    st.download_button(
+                        label="File is ready! Click here to download the audio",
+                        data=file,
+                        file_name=os.path.basename(audio_file),
+                        mime='audio/mp3'
+                    )
+    else:
+        st.subheader("Sorry, this video cannot be downloaded.")
